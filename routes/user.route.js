04e2cc023ca76b3e2eb677611aaf7ae0b90a2f5e
Esraamaher525 <esraamaher952@gmail.com>
2021-07-20 18:57:44 +0200

    first update

---
+++ routes/user.route.js
@@ -0,0 +1,4 @@
+const express = require('express')
+const router = new express.Router()
+//const userController = require('../app/controllers/user.controller')
+const auth = require('../app/middleware/auth')
---
+++ src/app.js
@@ -0,0 +1,13 @@
+const express= require('express')
+const cors = require('cors')
+
+require('dotenv').config()
+require('../database/connection')
+const userRoutes = require('../routes/user.route')
+const app = express()
+app.use(cors())
+
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+//app.use(userRoutes)
+module.exports = app
