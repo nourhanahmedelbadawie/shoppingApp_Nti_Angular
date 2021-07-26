ac38ba04a06cc77551d5e5457abbd7202ff4555a
Nourhan Ahmed <nourhanahmedelbadawie@gmail.com>
2021-07-26 23:23:27 +0200

    edit email-user file

--- app/middleware/email-user.js
+++ app/middleware/email-user.js
@@ -1,21 +1,18 @@
 const nodemailer = require('nodemailer')
-const emailUser=(email)=>{
+const emailUser=(mailOptions)=>{
     try{
     var transporter = nodemailer.createTransport({
         service: 'gmail',
+        Secure: false,
+        port: 587,
         auth: {
-          user: 'nourhanahmedelbadawe@gmail.com',
-          pass: '12119942010'
+          user: 'noreplay.OPSPv1@gmail.com',
+          pass: 'Matrix219'
         }
+
+
       });
-      
-      var mailOptions = {
-        from: 'nourhanahmedelbadawe@gmail.com',
-        to: email,
-        subject: 'promocode',
-        text: 'promocode added successfuly!'
-      };
-      
+     
       transporter.sendMail(mailOptions, function(error, info){
         if (error) {
           console.log(error);
