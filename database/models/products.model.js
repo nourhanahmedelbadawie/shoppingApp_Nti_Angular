6154214ff23ab1c19f202bab8b64144fb3458655
Esraamaher525 <esraamaher952@gmail.com>
2021-07-26 02:03:41 +0200

    updates

--- database/models/products.model.js
+++ database/models/products.model.js
@@ -1,10 +1,5 @@
 const mongoose = require('mongoose')
 const productSchema = new mongoose.Schema({
-    userId: {
-        type: mongoose.Schema.Types.ObjectId,
-        required:true,
-        ref:'User'
-    },
     category:{
         required:true,
         type:String,
